d8fc7849eeaf6818cd07bcc9ea07276197431117
Sinabb <86355292+Sinabb@users.noreply.github.com>
2023-10-30 18:03:03 +0900

    10 31 study

--- Bug.h
+++ Bug.h
@@ -5,20 +5,8 @@ class Bug : public Actor
 	// 화면 테두리에서 등장
 	// 일정 방향 이동
 	// 45도 단위로 전환
-	enum class Direction
-	{
-		UP,
-		UP_RIGHT,
-		RIGHT,
-		DOWN_RIGHT,
-		DOWN,
-		DOWN_LEFT,
-		LEFT,
-		UP_LEFT,
-		COUNT
-	};
-
-	Direction mDirection;
+	const D2D_POINT_2F UPVECTOR{ 0.0f, -1.0f };
+	float mRotation;
 	float mSteps;
 
 public:
